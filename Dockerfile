67c267d047ed876eafaf398117b9f53dc89d8aaa
linzhiqiang <linzhiqiang@gridsum.com>
2021-03-15 15:38:01 +0800

    refactor

--- Dockerfile
+++ Dockerfile
@@ -20,12 +20,13 @@ COPY . .
 RUN CGO_ENABLED=0 \
     GOOS=linux \
     GOARCH=amd64 \
-    go build -mod=readonly --gcflags="-N -l" -o gotestd cmd/main.go
+    go build -mod=readonly --gcflags="-N -l" -o gotestd iris/main.go
 
 FROM alpine:3.11
 
 WORKDIR /app
 
 COPY --from=builder /build/gotestd .
+COPY --from=builder /build/iris iris
 
 CMD ["/app/gotestd"]
--- iris/main.go
+++ iris/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func main() {
 	app := iris.Default()
@@ -13,6 +16,7 @@ func main() {
 		// Render template file: ./views/hello.html
 		ctx.View("hello.html")
 	})
+	app.Get("/metrics", iris.FromStd(promhttp.Handler()))
 	app.Get("/user/{id:uint64}", func(ctx iris.Context) {
 		userID, _ := ctx.Params().GetUint64("id")
 		ctx.Writef("User ID: %d", userID)
